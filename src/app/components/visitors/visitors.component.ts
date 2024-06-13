5b3476e0999d49357569e461dbc810694a588417
SalomaaAzzam <slmyzam85@gmail.com>
2024-06-14 01:37:08 +0300

    ticket

--- src/app/components/visitors/visitors.component.ts
+++ src/app/components/visitors/visitors.component.ts
@@ -3,7 +3,7 @@ import { RouterLink } from '@angular/router';
 import { MapMarker } from '@angular/google-maps';
 import { GoogleMapsModule } from '@angular/google-maps';
 import { NgxCountAnimationModule } from 'ngx-count-animation';
-
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-visitors',
@@ -17,7 +17,15 @@ export class VisitorsComponent implements OnInit {
   center: google.maps.LatLngLiteral = { lat: 30.05611, lng: 31.33078 };
   zoom: number = 15;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void { }
+
+  navigateToBooking() {
+    this.router.navigate(['/bookingTicket']);
+}
+
+
+
+
 }
