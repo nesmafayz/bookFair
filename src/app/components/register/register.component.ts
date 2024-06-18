6ae8b387b58e7b07b82d1d4fe6a3e8bc2b439f5f
nesmafayz <nesmafayez21@gmail.com>
2024-06-18 21:13:35 +0300

    trans

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,13 +1,27 @@
+import { formatCurrency } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink,ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  registerForm:FormGroup = new FormGroup({
+    name:new FormControl(null, [Validators.required, Validators.minLength(3)]),
+    email:new FormControl(null, [Validators.required, Validators.email]),
+    password:new FormControl(null, [Validators.required]),
+  })
+
+
+  handleRegister(registerForm:FormGroup)
+  {
+      console.log(registerForm);
+  }
 
 }
--- src/app/components/transports/transports.component.ts
+++ src/app/components/transports/transports.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TransportsService } from '../../services/transports.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-transports',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './transports.component.html',
   styleUrl: './transports.component.css'
 })
-export class TransportsComponent {
+export class TransportsComponent implements OnInit {
+  busLines: any[] = [];
+  constructor(private _transportService:TransportsService)
+  {
+
+  }
+  ngOnInit(): void {
+    this.busLines = this._transportService.getBusLines();
+  }
 
 }
