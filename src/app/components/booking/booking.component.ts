f98a3380c38943af5be7fc691d1be95262784041
nesmafayz <nesmafayez21@gmail.com>
2024-06-22 04:04:40 +0300

    updateUI

--- src/app/components/booking/booking.component.ts
+++ src/app/components/booking/booking.component.ts
@@ -1,12 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-booking',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './booking.component.html',
   styleUrl: './booking.component.css'
 })
 export class BookingComponent {
+  formData = {
+    name: '',
+    phone: '',
+    tickets: 1,
+    totalPrice: 5
+  };
 
+  ticketOptions: number[] = Array.from({ length: 20 }, (_, i) => i + 1);
+
+  updateTotalPrice() {
+    const ticketPrice = 5;
+    this.formData.totalPrice = this.formData.tickets * ticketPrice;
+  }
+
+  onSubmit() {
+    console.log('Form Data:', this.formData);
+    // Handle form submission logic here
+  }
 }
