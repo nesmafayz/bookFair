a5f44e7a6fe4ce35a1f96c67c0d98c57110819d7
nesmafayz <nesmafayez21@gmail.com>
2024-06-23 04:47:53 +0300

    register is done

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,27 +1,46 @@
 import { formatCurrency } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, Validators, ReactiveFormsModule,FormBuilder } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [RouterLink,ReactiveFormsModule],
+  imports: [RouterLink,ReactiveFormsModule,CommonModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
-  registerForm:FormGroup = new FormGroup({
-    name:new FormControl(null, [Validators.required, Validators.minLength(3)]),
-    email:new FormControl(null, [Validators.required, Validators.email]),
-    password:new FormControl(null, [Validators.required]),
-  })
+  registerForm:FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.registerForm = this.fb.group({
+      name: new FormControl(null, [Validators.required, Validators.minLength(3)]),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [
+        Validators.required,
+        Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\W).{8,}$/)
+      ]),
+      repassword: new FormControl(null, Validators.required)
+    }, { validator: this.repasswordMatch });
+  }
 
+  repasswordMatch(form: FormGroup) {
+    const passwordControl = form.get('password');
+    const repasswordControl = form.get('repassword');
+
+    if (passwordControl?.value === repasswordControl?.value) {
+      return null;
+    } else {
+      repasswordControl?.setErrors({ repasswordMatch: 'كلمتان السر غير متطابقتين' });
+      return { repasswordMatch: 'كلمتان السر غير متطابقتين' };
+    }
+  }
 
   handleRegister(registerForm:FormGroup)
   {
-      console.log(registerForm);
+      console.log(registerForm.value);
   }
 
 }
