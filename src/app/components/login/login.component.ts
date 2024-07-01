89e70e9014df776a01cd04532d9452b0de240538
esraamohammed44 <esraamohammed1112000@gmail.com>
2024-07-02 00:18:47 +0200

    edit login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { AuthService } from '../../services/auth.service'; 
+import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink,CommonModule, ReactiveFormsModule, FormsModule],
+  imports: [RouterLink, CommonModule, ReactiveFormsModule, FormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
   loginForm: FormGroup;
@@ -26,23 +27,20 @@ export class LoginComponent {
   login() {
     if (this.loginForm.valid) {
       const { email, password } = this.loginForm.value;
-      
-      // Log form data to console
-      console.log('Login form data:', { email, password });
-      
+
       this.authService.login(email, password).subscribe(
         success => {
           if (success) {
             // Navigate to Home upon successful login
             this.router.navigate(['/Home']);
-          } else {
-            this.errorMessage = 'Login failed. Please try again.';
-          }
+          } 
         },
         error => {
-          this.errorMessage = 'An error occurred. Please try again.';
+          this.errorMessage = 'An error occurred during login.';
         }
       );
+      // Log form data to console
+      console.log('Login form data:', { email, password });
     }
   }
 
