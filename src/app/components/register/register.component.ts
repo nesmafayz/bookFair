ea7c0889f96467cf5e4822ca22bb8812e8cdd80c
SalomaaAzzam <slmyzam85@gmail.com>
2024-07-02 20:57:06 +0300

    commit

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -3,21 +3,21 @@ import { FormControl, FormGroup, Validators, ReactiveFormsModule, ValidatorFn, A
 import { Router, RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { RegisterServiceService } from '../../services/register-service.service';
-import { GoogleSigninButtonModule } from '@abacritt/angularx-social-login';
-import { SocialAuthService } from "@abacritt/angularx-social-login";
+
 
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [RouterLink, ReactiveFormsModule, CommonModule,GoogleSigninButtonModule],
+  imports: [RouterLink, ReactiveFormsModule, CommonModule],
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']  
 })
 export class RegisterComponent{
   registerForm: FormGroup;
 
-  constructor(private _registerService:RegisterServiceService,private Router:Router,private authService: SocialAuthService) {
+  constructor(private _registerService:RegisterServiceService,
+    private Router:Router) {
     this.registerForm = new FormGroup({
       username: new FormControl(null, [Validators.required, Validators.minLength(3)]),
       Fullname: new FormControl(null, [Validators.required, Validators.minLength(3)]),
