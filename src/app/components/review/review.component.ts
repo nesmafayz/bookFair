42f1fbedf4aa569896c84756457605d9c420282f
nesmafayz <nesmafayez21@gmail.com>
2024-07-08 04:17:24 +0300

    reviewsAll

--- src/app/components/review/review.component.ts
+++ src/app/components/review/review.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { ReviewSectionComponent } from '../review-section/review-section.component';
+import { BookReviewsAllComponent } from '../book-reviews-all/book-reviews-all.component';
 
 
 @Component({
   selector: 'app-review',
   standalone: true,
-  imports: [ReviewSectionComponent],
+  imports: [ReviewSectionComponent,BookReviewsAllComponent],
   templateUrl: './review.component.html',
   styleUrl: './review.component.css'
 })
--- src/app/services/review.service.ts
+++ src/app/services/review.service.ts
@@ -1,17 +1,21 @@
-
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { environment } from '../../environments/environment.development';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReviewService {
+  private apiUrl = 'https://localhost:7175/api/Reviews';
 
-  constructor(private http:HttpClient) {}
-  getReviewById(ReviuewId: number): Observable<any> {
-    const url = `${environment.baseUrl}/api/Reviews/${ReviuewId}`;
+  constructor(private http: HttpClient) {}
+
+  getReviewById(reviewId: number): Observable<any> {
+    const url = `${this.apiUrl}/${reviewId}`;
     return this.http.get<any>(url);
-}
+  }
+
+  getAllReviews(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl);
+  }
 }
